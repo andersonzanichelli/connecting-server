a6b519d880e86882f96d885bddfb417bca306f8c
Anderson Zanichelli <andersonzanichelli@github.com>
2016-05-22 16:23:06 -0300

    Cors wtf

--- server.js
+++ server.js
@@ -136,6 +136,7 @@ connecting.dbOperations = function(params) {
 
 server.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "*");
     next();
 });
